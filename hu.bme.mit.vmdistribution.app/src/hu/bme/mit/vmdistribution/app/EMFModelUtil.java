c411dac4d9484a66a0d4f1a642f5694a3478a15c
Balázs Vincze <propump@utmail.net>
2015-11-05 00:56:10 +0100

    'model repr -> tasks' done, incquery stuff abandoned

--- hu.bme.mit.vmdistribution.app/src/hu/bme/mit/vmdistribution/app/EMFModelUtil.java
+++ hu.bme.mit.vmdistribution.app/src/hu/bme/mit/vmdistribution/app/EMFModelUtil.java
@@ -1,32 +1,26 @@
 package hu.bme.mit.vmdistribution.app;
 
 import java.io.File;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
 import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import org.eclipse.emf.common.util.URI;
-import org.eclipse.emf.ecore.resource.*;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.ecore.resource.ResourceSet;
 import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
 import org.eclipse.emf.ecore.xmi.impl.XMIResourceFactoryImpl;
-import org.eclipse.incquery.runtime.api.IncQueryEngine;
-import org.eclipse.incquery.runtime.exception.IncQueryException;
 
-import hu.bme.mit.vmdistribution.model.*;
-import hu.bme.mit.vmdistribution.model.queries.ComputersMatch;
-import hu.bme.mit.vmdistribution.model.queries.ComputersMatcher;
+import hu.bme.mit.vmdistribution.model.LabSystem;
+import hu.bme.mit.vmdistribution.model.VMDistributionPackage;
 
 public class EMFModelUtil {
 
 	private static final Logger logger = Logger.getLogger(EMFModelUtil.class.getName());
-	private IncQueryEngine engine;
+	//private IncQueryEngine engine;
 	
-	private ComputersMatcher computersmatcher;
+	//private ComputersMatcher computersmatcher;
 
-	@SuppressWarnings("deprecation")
 	public LabSystem loadModelInstance() {
 		VMDistributionPackage.eINSTANCE.eClass();
 		Resource.Factory.Registry reg = Resource.Factory.Registry.INSTANCE;
@@ -37,18 +31,20 @@ public class EMFModelUtil {
 		File myModel = new File("../hu.bme.mit.vmdistribution.model.tests/modeltest/My.vmdistribution");
 		Resource resource = resSet.getResource(URI.createURI(myModel.toURI().toString()), true);
 		
+		/*
 		try {
 			engine = IncQueryEngine.on(resource);
 			computersmatcher = ComputersMatcher.on(engine);
 		} catch (IncQueryException e) {
 			logger.log(Level.SEVERE, "ERROR ", e);
-		}
+		}*/
 		
 		LabSystem myLabSystem = (LabSystem) resource.getContents().get(0);
 		logger.log(Level.INFO, "[Model data loaded from " + myModel.getAbsolutePath() + "]");
 		return myLabSystem;
 	}
 	
+	/*
 	public List<Computer> query_GetAllComputers(){
 		List<Computer> result = new ArrayList<Computer>();
 		Collection<ComputersMatch> matches = computersmatcher.getAllMatches();
@@ -56,6 +52,6 @@ public class EMFModelUtil {
 			result.add(cm.getC());
 		}
 		return result;
-	}
+	}*/
 
 }
--- hu.bme.mit.vmdistribution.app/src/hu/bme/mit/vmdistribution/app/UseModel.java
+++ hu.bme.mit.vmdistribution.app/src/hu/bme/mit/vmdistribution/app/UseModel.java
@@ -2,7 +2,9 @@ package hu.bme.mit.vmdistribution.app;
 
 import java.io.File;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -22,6 +24,7 @@ public class UseModel {
 
 	private static final Logger logger_parent = Logger.getLogger("");
 	private static final Logger logger = Logger.getLogger(UseModel.class.getName());
+	private static LabSystem myLabSystem;
 
 	public static void init() {
 		System.setProperty("java.util.logging.SimpleFormatter.format", "%1$tY-%1$tm-%1$td %1$tH:%1$tM:%1$tS %4$s %2$s %5$s%6$s%n");
@@ -32,17 +35,12 @@ public class UseModel {
 		init();
 		logger.log(Level.INFO, "[Starting tasks.]");
 		EMFModelUtil emfutil = new EMFModelUtil();
-		LabSystem myLabSystem = emfutil.loadModelInstance();
+		myLabSystem = emfutil.loadModelInstance();
 		Lab goal = null;
 		for (Lab lab : myLabSystem.getLabs()){
 			if("mixed_test".equals(lab.getName())){
 				goal = lab;
 			}
-		}
-		
-		List<Computer> pcs = emfutil.query_GetAllComputers();
-		for (Computer computer : pcs) {
-			System.out.println(computer.getName());
 		}
 		
 		distribute(goal);
@@ -59,58 +57,115 @@ public class UseModel {
 		sshutil.copyFiles(files, destfolder);
 		sshutil.disconnect();
 	}
-
-	public static void distribute(Lab goal) {
-
-		List<VirtualMachine> seenVMs = new ArrayList<>();
-
-		// prepare vagrant vms & copy single copy of vms to torrent seed
+	
+	public static void copyTorrentFile(Host targetpc, String torrentfilename) {
+		StringBuilder command = new StringBuilder("~/shared/copy_torrent_from_seeder_to_leecher.sh ");
+		command.append(targetpc.getHostname()+" ");
+		command.append(String.valueOf(targetpc.getPassword())+" ");
+		command.append(torrentfilename);
+		remoteExec("seed", command.toString());
+	}
+	
+	public static void createTorrentFile(String vmzipname, String vmtorrentname){
+		StringBuilder command = new StringBuilder("~/shared/create_torrent.sh ");
+		Host host = Properties.getHostData("seed");
+		command.append(host.getHostname()+" ");
+		command.append(vmzipname+" ");
+		command.append(vmtorrentname);
+		remoteExec("seed", command.toString());
+	}
+	
+	public static void startSeeding(){
+		StringBuilder command = new StringBuilder("~/shared/start_seeding.sh ");
+		Host host = Properties.getHostData("seed");
+		command.append(host.getHostname()+" ");
+		command.append(String.valueOf(host.getPassword()));
+		remoteExec("seed", command.toString());
+	}
+	
+	public static void startLeeching(Host targetpc){
+		StringBuilder command = new StringBuilder("~/shared/start_leeching.sh ");
+		command.append(targetpc.getHostname()+" ");
+		command.append(String.valueOf(targetpc.getPassword()));
+		remoteExec("seed", command.toString());
+	}
+	
+	public static Map<VirtualMachine, String> copyVmsToSeed(Lab goal){
+		Map<VirtualMachine, String> vm_torrentfilename_map = new HashMap<>();
+		// prepare vagrant vms & copy single copy of vms to seed
 		logger.log(Level.INFO, "[Copying required VMs to seed.]");
 		for (ComputerConfig cfgitem : goal.getComputerconfigs()) {
 			for (VirtualMachine vm : cfgitem.getVirtualmachines()) {
 
-				if (seenVMs.contains(vm)) {
+				if (vm_torrentfilename_map.containsKey(vm)) {
 					continue;
 				}
 
+				File vmzipfile = null;
+				String vmtorrentname;
+				
 				if ("CustomVM".equals(vm.eClass().getName())) {
 					CustomVM customvm = (CustomVM) vm;
+					vmzipfile =  customvm.getVmZipArchive();
+					vmtorrentname = customvm.getVmZipArchive().getName().replace(".zip", ".torrent"); //TODO regex
 					copyVM("seed", customvm.getVmZipArchive(), "/home/vagrant/Downloads");
+					
 				} else {
 					Vagrant_VM vagrantvm = (Vagrant_VM) vm;
+					vmzipfile =  vagrantvm.getDistributionImage();
+					vmtorrentname = vagrantvm.getDistributionImage().getName().replace(".zip", ".torrent"); //TODO regex
 					prepareVagrantVM(vagrantvm);
 					copyVM("seed", vagrantvm.getDistributionImage(), "/home/vagrant/Downloads");
+					
 				}
-				seenVMs.add(vm);
+				createTorrentFile(vmzipfile.getName(), vmtorrentname);
+				vm_torrentfilename_map.put(vm, vmtorrentname);
 			}
 		}
 		
-		/*
-		//CHEAT:
-		List<Computer> targetpcs = new ArrayList<>();
-		targetpcs.add(goal.getComputerconfigs().get(0).getComputer());
-		targetpcs.add(goal.getComputerconfigs().get(1).getComputer());
-		//targetpcs.add(goal.getComputerconfig().get(2).getComputer());
+		return vm_torrentfilename_map;
+	}
+	
+	public static Map<Computer, List<VirtualMachine>> buildComputerToVMsMapFromLabSystem(LabSystem labsystem){
+		Map<Computer, List<VirtualMachine>> map = new HashMap<>();
+		for(Computer c : labsystem.getComputers()){
+			map.put(c, c.getVirtualmachines());
+		}
+		return map;
+	}
+	
+	public static Map<Computer, List<VirtualMachine>> buildComputerToVMsMapFromLab(Lab lab){
+		Map<Computer, List<VirtualMachine>> map = new HashMap<>();
+		for(ComputerConfig cfg : lab.getComputerconfigs()){
+			map.put(cfg.getComputer(), cfg.getVirtualmachines());
+		}
+		return map;
+	}
+	
+	public static void distribute(Lab goal) {
 		
-		logger.log(Level.INFO, "[Creating torrent file and starting seeding.]");
-		StringBuilder command = new StringBuilder("~/shared/begin_seed.sh ");
-		Host host = Properties.getHostData("seed");
-		command.append(host.getHostname()+" ");
-		command.append("testvm.zip ");
-		command.append("testvm.torrent");
-		remoteExec("seed", command.toString());
+		//copy vms to seed
+		Map<VirtualMachine, String> vm_torrentfilename_map = copyVmsToSeed(goal);
 		
-		logger.log(Level.INFO, "[Distribute torrent and start rtorrent on clients.]");
-		for (Computer pc : targetpcs){
-			command = new StringBuilder("~/shared/init_leecher.sh ");
-			host = Properties.getHostData(pc.getName());
-			command.append(host.getHostname()+" ");
-			command.append(String.valueOf(host.getPassword())+" ");
-			command.append("testvm.torrent");
-			remoteExec("seed", command.toString());
-		}*/
+		//start seeding
+		startSeeding();
 		
-		// updatemodel
+		//get compat vms to install
+		Map<Computer, List<VirtualMachine>> currentsetup = buildComputerToVMsMapFromLabSystem(myLabSystem);
+		Map<Computer, List<VirtualMachine>> goalsetup = buildComputerToVMsMapFromLab(goal);
+
+		Map<Computer, List<VirtualMachine>> vms_toinstall_notalreadyinstalled = getConfWithoutAlreadyInstalledVMs(currentsetup, goalsetup);
+		Map<Computer, List<VirtualMachine>> vms_toinstall_alsocompatible = getConfWithoutIncompatibleVMs(vms_toinstall_notalreadyinstalled);
+		
+		for(Computer pc : vms_toinstall_alsocompatible.keySet()){
+			List<VirtualMachine> vmstodistr = vms_toinstall_alsocompatible.get(pc);
+			for(VirtualMachine vm : vmstodistr){
+				copyTorrentFile(Properties.getHostData(pc.getName()), vm_torrentfilename_map.get(vm));
+			}
+			startLeeching(Properties.getHostData(pc.getName()));
+		}
+		
+		// TODO updatemodel
 	}
 
 	public static void prepareVagrantVM(Vagrant_VM vm) {
@@ -133,5 +188,68 @@ public class UseModel {
 		sshutil.disconnect();
 	}
 	
+	public static Map<Computer, List<VirtualMachine>> getConfWithoutAlreadyInstalledVMs(Map<Computer, List<VirtualMachine>> current_setup, Map<Computer, List<VirtualMachine>> goal_setup){
+		Map<Computer, List<VirtualMachine>> withoutalreadyinstalledvms = goal_setup;
+
+		for (Computer pc : current_setup.keySet()) {
+			if(withoutalreadyinstalledvms.containsKey(pc)){
+				withoutalreadyinstalledvms.get(pc).removeAll(pc.getVirtualmachines());
+			}
+		}
+		return withoutalreadyinstalledvms;
+	}
+	
+	public static Map<Computer, List<VirtualMachine>> getConfWithoutIncompatibleVMs(Map<Computer, List<VirtualMachine>> goal_setup){
+		Map<Computer, List<VirtualMachine>> goodsetup = new HashMap<>();
+
+		for(Computer pc: goal_setup.keySet()){
+			if(hasEnoughSpace(pc, goal_setup.get(pc))){
+				goodsetup.put(pc, new ArrayList<VirtualMachine>());
+				for (VirtualMachine vm :  goal_setup.get(pc)){
+					if(!isCompatible(pc, vm)){
+						goodsetup.get(pc).add(vm);
+					}
+				}
+			}
+		}
+		return goodsetup;
+	}
+	
+	public static double getUsedSpace(List<VirtualMachine> vms){
+		double space = 0;
+		for( VirtualMachine vm : vms){
+			space = space + vm.getRequirements().getReqSpace();
+		}
+		return space;
+	}
+	
+	public static boolean isCompatible(Computer pc, VirtualMachine vm){
+		boolean result = true;
+		if(!"x64".equals(pc.getArchitecture()) && !pc.getArchitecture().equals(vm.getRequirements().getReqArchi())){
+			result = false;
+			logger.log(Level.WARNING, "WARNING:_Computer:"+pc.getName()+" is not compatible with Virtual Machine:"+vm.getName()+", Architecture mismatch!");
+		}
+		if(pc.getInstalledRAM() < vm.getRequirements().getReqRAM()){
+			logger.log(Level.WARNING, "WARNING:_Computer:"+pc.getName()+" is not compatible with Virtual Machine:"+vm.getName()+", Not enough RAM!");
+		}
+		
+		return result;
+	}
+	
+	public static boolean hasEnoughSpace(Computer pc, List<VirtualMachine> vms){
+		boolean result;
+		
+		double pc_free_space = pc.getMaxSpaceForVMs() - getUsedSpace(pc.getVirtualmachines());
+		double vms_toinstall_disk_usage = getUsedSpace(vms);
+		
+		if(pc_free_space >= vms_toinstall_disk_usage){
+			result = true;
+		}else{
+			result = false;
+			logger.log(Level.WARNING, "WARNING:_Computer:"+pc.getName()+" does not have enough free space for the new VMs! Available: "+pc_free_space+" MB, Required: "+vms_toinstall_disk_usage+" MB");
+		}
+		
+		return result;
+	}
 	
 }
